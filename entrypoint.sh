9ef1f858b48ab4d9e48885c2fb84202fe67d362b
kaz <endo.kazuma1027@gmail.com>
2021-02-25 15:59:46 +0900

    remove papersize

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 set -eux
 
 pandoc $1 -o main.pdf --pdf-engine=lualatex \
-  -V papersize=a4 -V documentclass=bxjsreport \
+  -V documentclass=bxjsreport \
   -V classoption=pandoc -V classoption=jafont=ipaex
