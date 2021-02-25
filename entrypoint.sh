1e5fd307b53edb9a13ce80b6c07fe157d45f555d
kaz <endo.kazuma1027@gmail.com>
2021-02-25 17:36:45 +0900

    add haranoaji fonts

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,11 @@ set -eux
 # echo `kpsewhich --var-value=TEXMFHOME`
 # /github/home/texmf
 
+mkdir -p `kpsewhich --var-value=TEXMFLOCAL`/fonts/opentype/public/haranoaji
+
+git clone https://github.com/trueroad/HaranoAjiFonts.git `kpsewhich --var-value=TEXMFLOCAL`/fonts/opentype/public/haranoaji
+
+
 pandoc $1 -o main.pdf --pdf-engine=lualatex \
   -V documentclass=bxjsreport \
   -V classoption=pandoc
