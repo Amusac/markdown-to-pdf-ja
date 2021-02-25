1325bf41128003fa658ebdab16f5b46d80b9e722
kaz <endo.kazuma1027@gmail.com>
2021-02-25 16:20:10 +0900

    move tlmgr commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,17 @@
 
 set -eux
 
+tlmgr update --self && \
+    tlmgr install \
+        bxjscls \
+        bxwareki \
+        everyhook \
+        ipaex \
+        luatexja \
+        svn-prov \
+        type1cm && \
+    tlmgr update latex
+
 pandoc $1 -o main.pdf --pdf-engine=lualatex \
   -V documentclass=bxjsreport \
   -V classoption=pandoc -V classoption=jafont=ipaex
