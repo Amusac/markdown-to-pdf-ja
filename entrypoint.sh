9eaa9d00598ea2c996ee8b03854cac0ae85c0105
kaz <endo.kazuma1027@gmail.com>
2021-02-25 15:07:54 +0900

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 
-pandoc $1 -o main.pdf --latex-engine=lualatex
+pandoc $1 -o main.pdf --pdf-engine=lualatex
   -V papersize=a4 -V documentclass=bxjsreport
   -V classoption=pandoc -V classoption=jafont=ipaex
