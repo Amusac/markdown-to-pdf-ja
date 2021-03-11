1282f788adad9f6bd965255f09ca23bbc64e27ba
Kazma Endo <endo.kazuma1027@gmail.com>
2021-03-11 23:33:50 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,11 @@ set -eux
 # echo `kpsewhich --var-value=TEXMFHOME`
 # /github/home/texmf
 
-mkdir -p `kpsewhich --var-value=TEXMFLOCAL`/fonts/opentype/public/haranoaji
+# mkdir -p `kpsewhich --var-value=TEXMFLOCAL`/fonts/opentype/public/haranoaji
 
-git clone https://github.com/trueroad/HaranoAjiFonts.git `kpsewhich --var-value=TEXMFLOCAL`/fonts/opentype/public/haranoaji
+# git clone https://github.com/trueroad/HaranoAjiFonts.git `kpsewhich --var-value=TEXMFLOCAL`/fonts/opentype/public/haranoaji
 
-mktexlsr
+# mktexlsr
 
 pandoc $1 -o main.pdf --pdf-engine=lualatex \
   -V documentclass=bxjsreport \
